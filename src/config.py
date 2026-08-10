d75a9c79e253fd63415cbefa79f226e05f1b3eed
SidharthaManriquez44 <sidhmanr02@gmail.com>
2026-08-10 12:04:01 -0600

    update(config): update config with teh new features

--- src/config.py
+++ src/config.py
@@ -77,3 +77,63 @@ LABORATORY_PROBABILITIES = {
     TriageLevel.GREEN: 0.15,
     TriageLevel.BLUE: 0.02,
 }
+
+# -----------------------------------------------------------------------------
+# Laboratory
+# -----------------------------------------------------------------------------
+
+LABORATORY_SAMPLE_MIN = 3
+LABORATORY_SAMPLE_MODE = 5
+LABORATORY_SAMPLE_MAX = 10
+
+LABORATORY_TRANSPORT_MIN = 15
+LABORATORY_TRANSPORT_MODE = 22
+LABORATORY_TRANSPORT_MAX = 35
+
+LABORATORY_PROCESSING_MIN = 20
+LABORATORY_PROCESSING_MODE = 30
+LABORATORY_PROCESSING_MAX = 45
+
+# -----------------------------------------------------------------------------
+# Imaging
+# -----------------------------------------------------------------------------
+IMAGING_PROBABILITIES = {
+    TriageLevel.RED: 0.80,
+    TriageLevel.ORANGE: 0.75,
+    TriageLevel.YELLOW: 0.60,
+    TriageLevel.GREEN: 0.25,
+    TriageLevel.BLUE: 0.00,
+}
+
+IMAGING_MODALITY_PROBABILITIES = {
+    "xray": 0.583,
+    "ct": 0.305,
+    "ultrasound": 0.066,
+    "mri": 0.046,
+}
+
+# -----------------------------------------------------------------------------
+# Imaging duration parameters
+# -----------------------------------------------------------------------------
+
+XRAY_MIN = 25
+XRAY_MODE = 41
+XRAY_MAX = 83
+
+CT_MIN = 42
+CT_MODE = 67
+CT_MAX = 142
+
+ULTRASOUND_MIN = 30
+ULTRASOUND_MODE = 48
+ULTRASOUND_MAX = 84
+
+MRI_MIN = 108
+MRI_MODE = 156
+MRI_MAX = 222
+
+# -----------------------------------------------------------------------------
+# Fast Track
+# -----------------------------------------------------------------------------
+
+FAST_TRACK_CONSULTATION_FACTOR = 0.50
